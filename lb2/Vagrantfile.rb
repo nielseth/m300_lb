34b979b4cc0219bc4a3d12cad325cb7793f34b53
nielseth <niels.ethlehre@gmail.com>
2021-03-12 10:29:08 +0100

    In diesem Commit wurde das Vagrantfile.rb File gelöscht

--- lb2/Vagrantfile.rb
+++
@@ -1,5 +0,0 @@
-# -*- mode: ruby -*-
-# vi: set ft=ruby :
-
-# Mit diesem Vagrant File wird eine Virtuelle Maschine erstellt welche eine Datenbank hat und vom Host aus via MySQL Workbench erreicht 
-# werden kann. Diese Virtuelle Maschine hat eine Firewall und weitere Security Features. 
