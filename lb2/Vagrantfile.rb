6a05ce185d54fecf6f0d67ab4f92ec647bd35475
nielseth <niels.ethlehre@gmail.com>
2021-03-12 10:12:30 +0100

    In diesem Commit wurde der Kommentar zu beginn des Vagrantfiles hinzugefügt

---
+++ lb2/Vagrantfile.rb
@@ -0,0 +1,5 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+# Mit diesem Vagrant File wird eine Virtuelle Maschine erstellt welche eine Datenbank hat und vom Host aus via MySQL Workbench erreicht 
+# werden kann. Diese Virtuelle Maschine hat eine Firewall und weitere Security Features. 
