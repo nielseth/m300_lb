5439e5a258477e2fc34a4825f909c44456f8051e
nielseth <niels.ethlehre@gmail.com>
2021-03-19 09:10:40 +0100

    In diesem Commit wurde die Firewall installiert sowie Port 22 und 3306 nur für den Host geöffnet. Die Kommentare wurde auch entsprechend angepasst.

--- lb2/config.sh
+++ lb2/config.sh
@@ -8,8 +8,8 @@
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password admin'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password admin'
 
-# Hier wird der MySQL-Server installiert
-sudo apt-get install -y mysql-server 
+# Hier wird der MySQL-Server und ufw installiert
+sudo apt-get install -y mysql-server ufw
 
 # Mit dieser Zeile wird der MySQL Port geöffnet
 sudo sed -i -e"s/^bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
@@ -22,3 +22,9 @@ mysql -uroot -padmin <<%EOF%
 
 # Hier wird ein Restart des services gemacht damit die Konfigurationsänderungen übernommen werden
 sudo service mysql restart
+
+# Hier wird die Firewall der Box aktiviert und Port 22 und 3306 für den Host geöffnet
+sudo ufw allow from 192.168.55.1 to any port 22
+sudo ufw allow from 192.168.55.1 to any port 3306
+sudo ufw -f enable
+
